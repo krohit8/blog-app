0ec07af75f44d9aee6d2b2a94fd430a237733d98
krohit8 <kumarrairohit98@gmail.com>
2025-10-16 09:14:31 +0530

    change url to prod

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,3 +1,1 @@
-// export const BACKEND_URL = "https://bloggr-backend.kumarrairohit98.workers.dev"; 
-
-export const BACKEND_URL="http://127.0.0.1:8787"
+ export const BACKEND_URL = "https://bloggr-backend.kumarrairohit98.workers.dev"; 
