b86f1ec43a257e99dcdcc40ceb62c33d6a1b87f3
krohit8 <kumarrairohit98@gmail.com>
2025-10-18 19:02:11 +0530

    change URL

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,3 +1,1 @@
-//  export const BACKEND_URL = "https://bloggr-backend.kumarrairohit98.workers.dev"; 
-
-export const BACKEND_URL="http://localhost:8787"
+ export const BACKEND_URL = "https://bloggr-backend.kumarrairohit98.workers.dev"; 
