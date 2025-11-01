ab0957ed075834c22f20fdac9aa98f8feb3c30f0
krohit8 <kumarrairohit98@gmail.com>
2025-11-01 07:58:58 +0530

    feat: configure default options for React Query client to enhance caching and query behavior

--- frontend/src/react-query/queryProvider.tsx
+++ frontend/src/react-query/queryProvider.tsx
@@ -1,11 +1,20 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React from "react"
 
+const queryClient = new QueryClient({
+    defaultOptions:{
+        queries:{
+            staleTime: 1000 * 60 *5,
+            cacheTime: 1000 *60 *10,
+            retry: 2,
+            refetchOnWindowFocus: false
+        }
+    }
+})
 
 export const QueryProvider = ({children}:{children: React.ReactNode}) => {
-    const client =  new QueryClient()
     return (
-        <QueryClientProvider client={client}>
+        <QueryClientProvider client={queryClient}>
             {children}
         </QueryClientProvider>
     )
